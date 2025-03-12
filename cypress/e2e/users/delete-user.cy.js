c3e6b3beec759aa8bb4d019dd3d8aa01d8476960
renaldoit <122207239+renaldoit@users.noreply.github.com>
2025-03-12 22:29:44 +0700

    Delete cypress directory

--- cypress/e2e/users/delete-user.cy.js
+++
@@ -1,9 +0,0 @@
-// cypress/integration/users/delete-user.cy.js
-
-describe('Delete User API Tests', () => {
-    it('DELETE - Delete user', () => {
-      cy.request('DELETE', 'https://reqres.in/api/users/2').then((response) => {
-        expect(response.status).to.eq(204);
-      });
-    });
-  });
